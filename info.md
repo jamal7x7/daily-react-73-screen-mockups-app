7770fa7829b0ce42dd6d21b7129d7a7f50d5dc59
jamal7X7 <jamal7x7@gmail.com>
2018-10-04 21:53:58 +0000

    first commit

---
+++ info.md
@@ -0,0 +1,3 @@
+https://booksy.com/en-us/90220_ektor-the-barber_barber-shop_15755_riverview
+
+https://www.setmore.com/features/online-booking-page
---
+++ src/components/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import '../styles/App.css'
+
+const Header = () => (
+  <header className='App-header'>
+    <h1 className='App-title'> Screen Mockups App</h1>
+  </header>
+)
+
+const Main = (props) => (
+  <div className='game'>
+
+    
+
+  </div>
+)
+
+const Side = () => (
+  <div className='score'>
+
+      
+
+  </div>
+)
+
+
+
+class ScreenMockupsApp extends Component {
+  
+  state = {
+    
+  }
+
+
+
+  componentDidMount() {
+    
+  
+
+  }
+
+
+      render () {
+    return (
+      <div className='App-container'>
+
+        <Side />
+        <Main />
+       
+
+      </div>
+    )
+  }
+}
+
+const App = (props) => (
+  <div className='App'>
+    <Header />
+    <ScreenMockupsApp />
+
+  </div>
+)
+
+export default App
+
